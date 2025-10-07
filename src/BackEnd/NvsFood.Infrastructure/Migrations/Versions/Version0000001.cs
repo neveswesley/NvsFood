2afc34a6d5f637c6ea9b095d150d7507458b5bac
wesley neves <wellynvs@gmail.com>
2025-10-06 22:27:36 -0300

    feat: version created

---
+++ src/BackEnd/NvsFood.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace NvsFood.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.TABLE_USER, "Create table to save the users information")]
+public class Version0000001 : VersionBase
+{
+    public override void Up()
+    {
+        Create.Table("Users")
+            .WithColumn("Name").AsString(255).NotNullable()
+            .WithColumn("Email").AsString(255).NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable();
+    }
+}
