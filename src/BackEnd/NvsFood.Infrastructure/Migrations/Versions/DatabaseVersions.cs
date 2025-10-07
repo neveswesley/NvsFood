f26b3c54b1f1984f22403e818d0c78d10b21ae74
wesley neves <wellynvs@gmail.com>
2025-10-06 22:27:36 -0300

    feat: column created

---
+++ src/BackEnd/NvsFood.Infrastructure/Migrations/Versions/DatabaseVersions.cs
@@ -0,0 +1,6 @@
+﻿namespace NvsFood.Infrastructure.Migrations.Versions;
+
+public abstract class DatabaseVersions
+{
+    public const int TABLE_USER = 1;
+}
---
+++ src/BackEnd/NvsFood.Infrastructure/Migrations/Versions/VersionBase.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+
+namespace NvsFood.Infrastructure.Migrations.Versions;
+
+public abstract class VersionBase : ForwardOnlyMigration
+{
+    protected void CreateTable(string table)
+    {
+        Create.Table(table)
+            .WithColumn("id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("CreatedAt").AsDateTime().NotNullable()
+            .WithColumn("Active").AsBoolean().NotNullable();
+    }
+}
